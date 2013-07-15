6a8aec2f7d374d970a0d10f6531bf70853e51655
mohanrajr <mohanrajr@IMACHE052.(none)>
2013-07-15 12:52:13 +0530

    Alignment format.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
-	has_many :sessions
+  has_many :sessions
 end
